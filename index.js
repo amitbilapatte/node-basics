dbbe0c454189d818ea3882da8e9008644c12ca0a
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-11 19:47:45 +0530

    render html card dynamically from url

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const products = data.products;
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const product = products.find((p) => p.id === +id);
+    res.setHeader("Content-Type", "text/html");
+    let modifiedIndex = index
+      .replace("**title**", product.title)
+      .replace("**price**", product.price)
+      .replace("**source**", product.thumbnail)
+      .replace("**brand**", product.brand);
+    return res.end(modifiedIndex);
+  }
+
   switch (req.url) {
     case "/":
       res.setHeader("Content-Type", "text/html");
