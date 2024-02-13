5a6cffe8afadf972aa4c9c0ba0a500eabbc43c70
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-13 18:51:00 +0530

    Use body-parser middleware to send authentication via body in post request

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const products = data.products;
 
 const server = express();
 
+server.use(express.json());
+
 server.use((req, res, next) => {
   console.log(req.url);
   console.log(req.method);
@@ -21,12 +23,20 @@ const Auth = (req, res, next) => {
     res.send(401);
   }
 };
+const Authenticate = (req, res, next) => {
+  if (req.body.q) {
+    console.log(req.body);
+    next();
+  } else {
+    res.send(401);
+  }
+};
 // server.use(Auth);
 
 server.get("/", Auth, (req, res) => {
   res.json({ type: "GET" });
 });
-server.post("/", Auth, (req, res) => {
+server.post("/", Authenticate, (req, res) => {
   res.json({ type: "POST" });
 });
 server.put("/", (req, res) => {
