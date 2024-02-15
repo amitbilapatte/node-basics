95d570b548347832885a2af470388a7ac05429af
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-15 16:19:52 +0530

    chain all request to single server

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const server = express();
 server.use(express.json());
 server.use(express.static("public"));
 
-server.get("/products", productController.getAll);
-
-server.post("/add-product", productController.create);
-server.get("/products/:id", productController.get);
-server.put("/products/:id", productController.replace);
-server.patch("/products/:id", productController.update);
-server.delete("/products/:id", productController.remove);
+server
+  .get("/products", productController.getAll)
+  .post("/add-product", productController.create)
+  .get("/products/:id", productController.get)
+  .put("/products/:id", productController.replace)
+  .patch("/products/:id", productController.update)
+  .delete("/products/:id", productController.remove);
 
 server.listen("8000");
