b2f8c9522c794036a88aa5bbd38b6700a70a9417
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-15 16:17:23 +0530

    modify index js to move code to controller file

--- index.js
+++ index.js
@@ -1,57 +1,18 @@
 const express = require("express");
 const fs = require("fs");
-
 const index = fs.readFileSync("index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = data.products;
+const productController = require("./controller/product");
 
 const server = express();
-
 server.use(express.json());
-
 server.use(express.static("public"));
 
-//CREATE /POST
-server.post("/add-product", (req, res) => {
-  console.log(req.body);
-  products.push(req.body);
-  res.json(req.body);
-});
+server.get("/products", productController.getAll);
 
-//READ /Get /product/:id
-server.get("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const product = products.find((p) => p.id === id);
-
-  product ? res.json(product) : res.json({ error: "invalid id" });
-});
-
-//UPDATE /PUT /product/:id //To update whole object with new body
-server.put("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json(req.body);
-});
-//UPDATE /PATCH /product/:id //To only update few params in object from body
-server.patch("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[[productIndex]];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json(req.body);
-});
-
-//DELETE /delete /product/:id
-server.delete("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1);
-  res.status(200).json("mission successful");
-});
-
-server.get("/products", (req, res) => {
-  res.json(products);
-});
+server.post("/add-product", productController.create);
+server.get("/products/:id", productController.get);
+server.put("/products/:id", productController.replace);
+server.patch("/products/:id", productController.update);
+server.delete("/products/:id", productController.remove);
 
 server.listen("8000");
