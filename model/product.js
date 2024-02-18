c588d6fb471ae79735143b01e1e353339e10828f
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-19 00:34:38 +0530

    Add validations for schema

--- model/product.js
+++ model/product.js
@@ -3,14 +3,14 @@ const { Schema } = mongoose;
 
 //Schema
 const productSchema = new Schema({
-  title: String,
+  title: { type: String, required: true },
   description: String,
-  price: Number,
-  discountPercentage: Number,
-  rating: Number,
-  brand: String,
-  category: String,
-  thumbnail: String,
+  price: { type: Number, required: true, min: 1, max: [3000, "Don't be लालची"] },
+  discountPercentage: { type: Number, min: 0, max: [50, "तुम्ही मोफत देत आहात का?"] },
+  rating: { type: Number, min: [0, "rating can't be < 0"], max: [5, "rating can't be > 5"] },
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  thumbnail: { type: String },
   images: [String],
 });
 
