5cc2ee4f82d3b1533d3fdc7125606ca13804aa7b
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-19 23:48:19 +0530

    Make title unique in schema so that any new record can't be entered with existing title in table

--- model/product.js
+++ model/product.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 //Schema
 const productSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   description: String,
   price: { type: Number, required: true, min: 1, max: 3000 },
   discountPercentage: { type: Number, min: 0, max: 50 },
