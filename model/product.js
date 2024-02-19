2cfc00fe10467c0a0971eae286408d15a1470ee7
Amit Bilapatte <agbilapatte78@gmail.com>
2024-02-19 17:20:27 +0530

    remove error messages set by me

--- model/product.js
+++ model/product.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 const productSchema = new Schema({
   title: { type: String, required: true },
   description: String,
-  price: { type: Number, required: true, min: 1, max: [3000, "Don't be लालची"] },
-  discountPercentage: { type: Number, min: 0, max: [50, "तुम्ही मोफत देत आहात का?"] },
-  rating: { type: Number, min: [0, "rating can't be < 0"], max: [5, "rating can't be > 5"] },
+  price: { type: Number, required: true, min: 1, max: 3000 },
+  discountPercentage: { type: Number, min: 0, max: 50 },
+  rating: { type: Number, min: 0, max: 5 },
   brand: { type: String, required: true },
   category: { type: String, required: true },
   thumbnail: { type: String },
